19f9ffda51ef5b347421be047853e613a026fc95
Andrew Alteri <andrewsalteri@gmail.com>
2020-05-05 17:57:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-  
+
   WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -16,7 +16,6 @@ class TicTacToe
 end
 
 def display_board
-  puts @board 
+  puts @board
 end
 end
-
